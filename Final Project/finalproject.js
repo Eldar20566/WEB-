fbc5532adc2684ed1c3ba9cec7a0ac80ae2691a2
Eldar20566 <62555407+Eldar20566@users.noreply.github.com>
2020-03-23 20:40:03 +0600

    Add files via upload

---
+++ Final Project/finalproject.js
@@ -0,0 +1,412 @@
+function EN(){
+    document.getElementById('glav1').innerHTML='Home';
+    document.getElementById('glav2').innerHTML='Store';
+    document.getElementById('glav3').innerHTML='License';
+    document.getElementById('slogan').innerHTML='FOR SHOPPING - AT "WEB PORTAL"';
+    var tovar=document.getElementsByClassName('nametovar');
+    var change=tovar[0].innerHTML='Your tovar';
+    var change=tovar[1].innerHTML='Your tovar';
+    var change=tovar[2].innerHTML='Your tovar';
+    var change=tovar[3].innerHTML='Your tovar';
+    document.getElementById('catalog').innerHTML='Catalog';
+    document.getElementById('time').innerHTML='Time to Buy';
+    document.getElementById('h2info').innerHTML='«WEB-PORTAL»: our history>';
+    document.getElementById('qwe').innerHTML='Contacts';
+    document.getElementById('meken').innerHTML='Jandarbekova street 1, Aksukent, 160800, Kazakhstan';
+    document.getElementById('inname').placeholder='Name';
+    document.getElementById('insurname').placeholder='Surname';
+    document.getElementById('innumber').placeholder='Number';
+    document.getElementById('inadress').placeholder='Adress';
+    document.getElementById('intheme').placeholder='Theme';
+    document.getElementById('incomment').placeholder='Add comment...'
+
+}
+function black(){
+    document.body.style.backgroundColor ="black";
+    document.getElementById("heading").style.backgroundColor ="black";
+    document.getElementById("header").style.backgroundColor ="black";
+    document.getElementById("companyname").style.color="white";
+    document.getElementById('moon').src="moonwhite.png";
+    document.getElementById('sun').src="sunwhite.png";
+    document.getElementById("RUS").style.color="white";
+    document.getElementById("ENG").style.color="white";
+    document.getElementById("heding2").style.backgroundColor ="black";
+    document.getElementById("heding2-1").style.backgroundColor ="black";
+    document.getElementById("glav1").style.color="white";
+    document.getElementById('glav2').style.color='white';
+    document.getElementById('glav3').style.color='white';
+    document.getElementById('korzina').src="korzinawhite.png";
+    document.getElementById("glav1").style.color="white";
+    document.getElementById("glav1").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav1").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    document.getElementById("glav2").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav2").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    document.getElementById("glav3").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav3").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    var tovar=document.getElementsByClassName('nametovar');
+    var change= tovar[0].style.color="white";
+    var change= tovar[1].style.color="white";
+    var change= tovar[2].style.color="white";
+    var change= tovar[3].style.color="white";
+    
+    var price=document.getElementsByClassName('discount');
+    var pricech= price[0].style.color="white";
+    var pricech= price[1].style.color="white";
+    var pricech= price[2].style.color="white";
+    var pricech= price[3].style.color="white";
+    
+    document.getElementById('bgcatalog').style.backgroundColor='black';
+    document.getElementById('bgcatalog').style.borderTop ='1px solid white';
+    document.getElementById('catalog').style.color='white';
+    document.getElementById('time').style.color='white';
+    document.getElementById('info').style.backgroundColor='black';
+    document.getElementById('h2info').style.color='white';
+    document.getElementById('pinfo').style.color='white';
+    document.getElementById('preinfo').style.color='white';
+    document.getElementById('Time').style.backgroundColor='black';
+    document.getElementById('Time').style.borderBottom='1px solid white';
+    document.getElementById('pTime').style.color='white';
+    document.getElementById('pTime1').style.color='white';
+    document.getElementById('Date').style.color='white';
+    document.getElementById('Questions').style.backgroundColor='black';
+    document.getElementById('qwe').style.color='white';
+    document.getElementById('meken').style.color='white';
+}
+
+function black2(){
+    var price=document.getElementsByClassName('discount');
+    var pricech= price[0].style.color="white";
+    var pricech= price[1].style.color="white";
+    var pricech= price[2].style.color="white";
+    var pricech= price[3].style.color="white";
+    var pricech= price[4].style.color="white";
+    var pricech= price[5].style.color="white";
+    var pricech= price[6].style.color="white";
+    var pricech= price[7].style.color="white";
+    var pricech= price[8].style.color="white";
+    var pricech= price[9].style.color="white";
+    var pricech= price[10].style.color="white";
+    var pricech= price[11].style.color="white";
+    var tovar=document.getElementsByClassName('nametovar');
+    var change= tovar[0].style.color="white";
+    var change= tovar[1].style.color="white";
+    var change= tovar[2].style.color="white";
+    var change= tovar[3].style.color="white";
+    var change= tovar[4].style.color="white";
+    var change= tovar[5].style.color="white";
+    var change= tovar[6].style.color="white";
+    var change= tovar[7].style.color="white";
+    var change= tovar[8].style.color="white";
+    var change= tovar[9].style.color="white";
+    var change= tovar[10].style.color="white";
+    var change= tovar[11].style.color="white";
+    document.body.style.backgroundColor ="black";
+    document.getElementById("heading").style.backgroundColor ="black";
+    document.getElementById("header").style.backgroundColor ="black";
+    document.getElementById("companyname").style.color="white";
+    document.getElementById('moon').src="moonwhite.png";
+    document.getElementById('sun').src="sunwhite.png";
+    document.getElementById("RUS").style.color="white";
+    document.getElementById("ENG").style.color="white";
+    document.getElementById("heding2").style.backgroundColor ="black";
+    document.getElementById("heding2-1").style.backgroundColor ="black";
+    document.getElementById("glav1").style.color="white";
+    document.getElementById('glav2').style.color='white';
+    document.getElementById('glav3').style.color='white';
+    document.getElementById('korzina').src="korzinawhite.png";
+    document.getElementById("glav1").style.color="white";
+    document.getElementById("glav1").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav1").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    document.getElementById("glav2").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav2").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    document.getElementById("glav3").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav3").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+}
+
+function blackotovare(){
+    document.getElementById('vashtovar').style.color='white';
+    document.getElementById('articul').style.color='white';
+    var def=document.getElementsByClassName('definition');
+    var definition= def[0].style.color="white";
+    var definition= def[4].style.color="white";
+    var definition= def[6].style.color="white";
+    var definition= def[3].style.color="white";
+
+    var otovare=document.getElementsByClassName('otovare');
+    var tovar= otovare[0].style.color="white";
+    var tovar= otovare[1].style.color="white";
+    var tovar= otovare[2].style.color="white";
+    document.body.style.backgroundColor ="black";
+    document.getElementById("heading").style.backgroundColor ="black";
+    document.getElementById("header").style.backgroundColor ="black";
+    document.getElementById("companyname").style.color="white";
+    document.getElementById('moon').src="moonwhite.png";
+    document.getElementById('sun').src="sunwhite.png";
+    document.getElementById("RUS").style.color="white";
+    document.getElementById("ENG").style.color="white";
+    document.getElementById("heding2").style.backgroundColor ="black";
+    document.getElementById("heding2-1").style.backgroundColor ="black";
+    document.getElementById("glav1").style.color="white";
+    document.getElementById('glav2').style.color='white';
+    document.getElementById('glav3').style.color='white';
+    document.getElementById('korzina').src="korzinawhite.png";
+    document.getElementById("glav1").style.color="white";
+    document.getElementById("glav1").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav1").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    document.getElementById("glav2").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav2").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    document.getElementById("glav3").onmouseenter=function(){
+        this.style.backgroundColor='white';
+        this.style.color='black';
+    }
+    document.getElementById("glav3").onmouseleave=function(){
+        this.style.backgroundColor='black';
+        this.style.color='white';
+        this.style.transition='1.5s';
+    }
+    document.getElementById("minus").src='minuswhite.png';
+    document.getElementById("plus").src='pluswhite.png';
+    document.getElementById("KOLVO").style.color='white';
+    document.getElementById("del").style.color='green';
+    document.getElementById("1000").style.color='green';
+    document.getElementById("850").style.color='white';
+    document.getElementById('qwerty').style.color='black';
+    document.getElementById('qwerty').style.backgroundColor='white';
+}
+
+function add(){
+var x=document.getElementById('inputc').value;
+x++;
+if(x>=0&&x<=99)
+document.getElementById('inputc').value= x;
+}
+
+function minus(){
+    var x=document.getElementById('inputc').value;
+    x--;
+    if(x>=0){
+    document.getElementById('inputc').value= x;
+    }
+
+}
+
+function check(){
+    var x=document.getElementById("inname");
+    var y=document.getElementById("insurname");
+    var z=document.getElementById('incomment');
+
+    let name = document.getElementById("inname").value;
+    let surname = document.getElementById('insurname').value;
+    let text=document.getElementById('incomment').value;
+    
+    if (name.length<1){
+        alert("Please fill First Name");
+        x.style.backgroundColor='yellow';
+        return false;
+    }
+    else if(name.length>20){
+        alert("No more than 20 symbols");
+        x.style.backgroundColor='yellow';
+        return false
+    }
+    else{
+        x.style.backgroundColor='white';
+        if(surname.length<1){
+            alert("Please fill Repeat Name");
+            y.style.backgroundColor='yellow';
+            return false;
+        }
+        else if(surname.length>20){
+            alert("No more than 20 symbols");
+            y.style.backgroundColor='yellow';
+            return false;
+        }
+        
+        
+            else{
+                y.style.backgroundColor='white';
+                if(text.length>250){
+                    alert("No more than 250 symbols");
+                    z.style.backgroundColor='yellow';
+                    return false;
+                }
+                else{
+                    z.style.backgroundColor='white';
+                    return true;
+                }
+                
+            
+        
+        }
+    }
+}
+function number(){
+    document.getElementById('telnum').value='+77';
+}
+
+function checkfirstsymbol(letters)
+{
+    let numbers=[1,2,3,4,5,6,7,8,9,0];
+
+    for(let l=0;l<numbers.length;l++)
+    {
+        if (letters[0]==numbers[l])
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+}
+
+
+
+
+
+function check1(){
+    
+    var name=document.getElementById('namecard');
+    var surname=document.getElementById('surnamecard');
+    var number=document.getElementById('numbercard');
+    var month=document.getElementById('month');
+    var year=document.getElementById('year');
+    var cvc=document.getElementById('cvc');
+    var telnum=document.getElementById('telnum');
+    var email=document.getElementById('gmail');
+    var iin=document.getElementById('iin');
+
+    let ati=document.getElementById('namecard').value;
+    let tegi=document.getElementById('surnamecard').value;
+    
+    if(name.value.length<1){
+        alert("Please fill Name on latin");
+        name.style.backgroundColor='yellow';
+        return false;
+    }
+    else if (name.value.length>20){
+        alert("No more than 20 words");
+        name.style.backgroundColor='yellow';
+        return false;
+    }
+    else if(checkfirstsymbol(ati)==false){
+        alert("You cannot use numbers!");
+        name.style.backgroundColor='yellow';
+        return false;
+    }
+    else{
+        name.style.backgroundColor='white';
+        if(surname.value.length<1){
+            alert("Please fill Name on latin!");
+            surname.style.backgroundColor='yellow';
+            return false;
+        }
+        else if (surname.value.length>20){
+            alert("No more than 20 words");
+            surname.style.backgroundColor='yellow';
+            return false;
+        }
+        else if(checkfirstsymbol(tegi)==false){
+            alert("You cannot numbers!");
+            surname.style.backgroundColor='yellow';
+            return false;
+        }
+        else{
+            surname.style.backgroundColor='white';
+            if(number.value.length!=16){
+            alert("False number card!");
+            number.style.backgroundColor='yellow';
+            return false;
+            }
+            else{
+                number.style.backgroundColor='white';
+                if(month.value<03&&year.value<=2020){
+                    alert("Card expired!");
+                    month.style.backgroundColor='yellow';
+                    year.style.backgroundColor='yellow';
+                    return false; 
+                }
+                else{
+                    month.style.backgroundColor='white';
+                    year.style.backgroundColor='white';
+                    if(cvc.value.length>3){
+                        cvc.style.backgroundColor='yellow';
+                        alert("False CVC!");
+                        return false;
+                    }
+                    else{
+                        cvc.style.backgroundColor='white';
+                        if(telnum.value.lenght>12){
+                            telnum.style.backgroundColor='yellow';
+                            alert("False telefon number!");
+                            return false;
+                        }
+                        else{
+                            telnum.style.backgroundColor='white';
+                            return true;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
